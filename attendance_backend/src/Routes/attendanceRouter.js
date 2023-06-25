cd3c05ec5751ee212b1c42c096468c8e2acb5f3b
sandeep dhakal <dhakalsandeep71@gmail.com>
2023-06-25 09:22:31 +0545

    month

--- attendance_backend/src/Routes/attendanceRouter.js
+++ attendance_backend/src/Routes/attendanceRouter.js
@@ -1,13 +1,25 @@
 import { Router } from "express";
-import {  getAllAttendance, studentList, submitAttendance } from "../controllers/attendanceController.js";
+import {
+  getAllAttendance,
+  studentList,
+  submitAttendance,
+} from "../controllers/attendanceController.js";
 import { isAuthenticated } from "../middleware/isAuthenticated.js";
 import { isAuthorized } from "../middleware/isAuthorized.js";
 
-let attendanceRouter = Router()
+let attendanceRouter = Router();
 
-attendanceRouter.route("/:batchId").get(isAuthenticated,isAuthorized,getAllAttendance)
-attendanceRouter.route("/studentList/:batchId").get(isAuthenticated,isAuthorized,studentList)
-attendanceRouter.route("/submit/:batchId").post(isAuthenticated,isAuthorized,submitAttendance)
-
+attendanceRouter
+  .route("/:batchId")
+  .get(isAuthenticated, isAuthorized, getAllAttendance);
+attendanceRouter
+  .route("/studentList/:batchId")
+  .get(isAuthenticated, isAuthorized, studentList);
+attendanceRouter
+  .route("/submit/:batchId")
+  .post(isAuthenticated, isAuthorized, submitAttendance);
+attendanceRouter
+  .route("/:batchId/:month")
+  .get(isAuthenticated, isAuthorized, submitAttendance);
 
 export default attendanceRouter;
